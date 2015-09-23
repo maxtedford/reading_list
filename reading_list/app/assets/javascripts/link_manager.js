6dd6fd28a9b819a3965cbdce4bc7e759e4787407
Max Tedford <max.tedford@gmail.com>
2015-09-23 15:32:13 -0600

    almost there with editing status

---
+++ reading_list/app/assets/javascripts/link_manager.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+  bindSubmitButton();
+  bindLinkEditLinks();
+});
+
+function bindSubmitButton() {
+  $('#submit-button').on('click', function(event) {
+    event.preventDefault();
+    var $urlField = $('#link-url');
+    var $url = $urlField.val();
+    $urlField.val("");
+    createNewLink($url);
+  })
+}
+
+function bindLinkEditLinks() {
+  $('.link-editor').on('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var $li = $(this).closest('li');
+    var $id = $li.data().id;
+    updateLink($id, $li);
+  })
+}
+
+function createNewLink(url) {
+  var $linkList = $('.link-list');
+  $.ajax({
+    url: '/links/',
+    method: 'POST',
+    data: { url: url },
+    success: function() {
+      $linkList.append(
+        '<li><a href="' + url + '">' + url + '</a>'
+        + '<br>'
+        + 'unread '
+        + '<a href="">Mark as read</a>'
+      )
+    }
+  })
+}
+
+function updateLink(id, li) {
+  $.ajax({
+    url: '/links/' + id,
+    method: 'PUT',
+    data: { id: id },
+    complete: function() {
+      reRenderLink(li);
+    }
+  })
+}
+
+function reRenderLink(li) {
+  if(li.parent().data().id === "unread-links"){
+    $('#read-links').append(li)
+    bindLinkEditLinks()
+  } else {
+    $('#unread-links').append(li)
+    bindLinkEditLinks()
+  }
+  li.remove();;
+}
